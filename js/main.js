cceb19d5a68eb2c03d90fa585e525ca2fc2a0bda
kelsonpy <146881834+kelsonpy@users.noreply.github.com>
2023-11-29 14:24:32 -0500

    initial setup

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
 // generates the grid
 
-document.addEventListener('DOMContentLoaded', function() {
-    const gridSize = 3; // Size of the grid
-    const gridContainer = document.getElementById('kenken-grid');
-
-    for (let row = 0; row < gridSize; row++) {
-        const rowDiv = document.createElement('div');
-        rowDiv.className = 'grid-row';
-
-        for (let col = 0; col < gridSize; col++) {
-            const cellDiv = document.createElement('div');
-            cellDiv.className = 'grid-cell';
-            cellDiv.id = `cell-${row}-${col}`;
-            rowDiv.appendChild(cellDiv);
+        // Extract puzzle size from URL parameters
+        function getParameterByName(name) {
+            name = name.replace(/[\[\]]/g, '\\$&');
+            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(window.location.href);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
         }
 
-        gridContainer.appendChild(rowDiv);
-    }
-});
+        // Generate the grid
+        function generateGrid(size) {
+            var grid = document.getElementById('kenken-grid');
+            grid.innerHTML = ''; // Clear existing grid
+
+            for (var i = 0; i < size; i++) {
+                var row = document.createElement('div');
+                row.className = 'grid-row';
+                for (var j = 0; j < size; j++) {
+                    var cell = document.createElement('div');
+                    cell.className = 'grid-cell';
+                    row.appendChild(cell);
+                }
+                grid.appendChild(row);
+            }
+        }
+
+        // On load
+        window.onload = function() {
+            var size = getParameterByName('puzzleSize'); // Get size from URL
+            if (size) generateGrid(size); // Generate grid if size is provided
+        };
