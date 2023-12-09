2ab1dff9137cb12f6d2ff91652406bf4c8a90916
kelsonpy <146881834+kelsonpy@users.noreply.github.com>
2023-12-08 23:20:08 -0500

    Created the layout for the website     HTML & css
    Will begin working on interaction via JS

--- js/main.js
+++ js/main.js
@@ -1,34 +1,5 @@
-// generates the grid
-
-        // Extract puzzle size from URL parameters
-        function getParameterByName(name) {
-            name = name.replace(/[\[\]]/g, '\\$&');
-            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-                results = regex.exec(window.location.href);
-            if (!results) return null;
-            if (!results[2]) return '';
-            return decodeURIComponent(results[2].replace(/\+/g, ' '));
-        }
-
-        // Generate the grid
-        function generateGrid(size) {
-            var grid = document.getElementById('kenken-grid');
-            grid.innerHTML = ''; // Clear existing grid
-
-            for (var i = 0; i < size; i++) {
-                var row = document.createElement('div');
-                row.className = 'grid-row';
-                for (var j = 0; j < size; j++) {
-                    var cell = document.createElement('div');
-                    cell.className = 'grid-cell';
-                    row.appendChild(cell);
-                }
-                grid.appendChild(row);
-            }
-        }
-
-        // On load
-        window.onload = function() {
-            var size = getParameterByName('puzzleSize'); // Get size from URL
-            if (size) generateGrid(size); // Generate grid if size is provided
-        };
+var puzzle_1 = [
+    [1, 2, 3],
+    [3, 1, 2],
+    [2, 3, 1]
+]
