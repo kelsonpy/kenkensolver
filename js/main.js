51934d82e3d4960ffba91809cef5622d1325d975
kelsonpy <146881834+kelsonpy@users.noreply.github.com>
2023-12-09 04:18:52 -0500

    Implemented JS portion; user can now  enter their answers and check for correctness. The website is  effectively finished.
     To do: We need to add more puzzles, begin error checking
     and handling, and improve overall aesthetics

--- js/main.js
+++ js/main.js
@@ -4,15 +4,7 @@ var solvedPuzzle_1 = [
     [2, 3, 1]
 ]
 
-var solvePuzzlesContainer = [solvedPuzzle_1]
 
-var targeScore_1 = [
-
-]
-
-var activePuzzle = [0]
-
-//
 document.getElementById('submitPuzzle').addEventListener('click', function(){
     var puzzleSize = 3;
     var userSolution = [];
@@ -22,14 +14,39 @@ document.getElementById('submitPuzzle').addEventListener('click', function(){
         for (var col = 0; col < puzzleSize; col++) {
             var cellIndex = row * puzzleSize + col + 1;
             var inputValue = document.querySelector(`.parent div:nth-child(${cellIndex}) input`).value;
-            userSolution[row].push(parseInt(inputValue, 10) || 0); // Parses the input value as a number, defaults to 0 if empty
+            userSolution[row].push(parseInt(inputValue, 10) || 0); 
         }
     }
 
     console.log(userSolution);
+
+    var isCorrect = checkSolution(userSolution, solvedPuzzle_1);
+    
+    if (isCorrect) {
+        var returnToMain = confirm("Congratulations, you completed the puzzle! Would you like to play again?");
+    
+        if (returnToMain) {
+            window.location.href = 'index.html';
+        }
+    } else {
+        alert("Incorrect, please try again");
+    }
 });
 
-function checkSolution(user, defaultPuzzle){
+function checkSolution(userSolution, defaultPuzzle) {
+    if (userSolution.length !== defaultPuzzle.length) {
+        return false;
+    }
+    for (var row = 0; row < userSolution.length; row++) {
+        if (userSolution[row].length !== defaultPuzzle[row].length) {
+            return false;
+        }
 
-    for (var col = 0; col < userSolution)
+        for (var col = 0; col < userSolution[row].length; col++) {
+            if (userSolution[row][col] !== defaultPuzzle[row][col]) {
+                return false; 
+            }
+        }
+    }
+    return true;
 }
